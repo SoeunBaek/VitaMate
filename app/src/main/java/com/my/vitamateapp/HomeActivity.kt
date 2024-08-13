20be8257fae3dbbbe81cfbbc8e9023e515ca69f1
soeun01 <qorthdms66@naver.com>
2024-08-14 00:25:18 +0900

    Add : 식단|영양제|물 프레그먼트로 전환 *영양제 관련해서 구현 예정

--- app/src/main/java/com/my/vitamateapp/HomeActivity.kt
+++ app/src/main/java/com/my/vitamateapp/HomeActivity.kt
@@ -8,59 +8,57 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.kakao.sdk.user.UserApiClient
 import com.my.vitamateapp.databinding.ActivityHomeBinding
-import com.my.vitamateapp.foodRecord.FoodRecord
+import com.my.vitamateapp.intakeFragment.IntakeFragment
 import com.my.vitamateapp.registerPage.MainActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
 
-
-        // 아침 식단 기록 페이지로 이동
-        binding.breakfastRecord.setOnClickListener {
-            val intent = Intent(this, FoodRecord::class.java)
-            startActivity(intent)
+        // Intake 프래그먼트를 동적으로 추가
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.intake_fragment_container, IntakeFragment())
+                .commit()
         }
 
-        //로그아웃 버튼 클릭 시 로그아웃
-        binding.kakaoLogoutButton.setOnClickListener{
+        // 로그아웃 버튼 클릭 시 로그아웃
+        binding.kakaoLogoutButton.setOnClickListener {
             vitamate_logout()
         }
 
-        //계정탈퇴 버튼 클릭 시 계정삭제
+        // 계정탈퇴 버튼 클릭 시 계정삭제
         binding.deleteAccountButton.setOnClickListener {
             deleteAccount()
         }
     }
 
-
-    /*함수 구현*/
-    private fun vitamate_logout(){
+    /* 함수 구현 */
+    private fun vitamate_logout() {
         // 로그아웃
         UserApiClient.instance.logout { error ->
             if (error != null) {
                 Log.e(ContentValues.TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
-            }
-            else {
+            } else {
                 Log.i(ContentValues.TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
                 startActivity(Intent(this, MainActivity::class.java))
-                finish() //로그아웃 성공하면 다시 앱 초기 화면으로 이동.
+                finish() // 로그아웃 성공하면 다시 앱 초기 화면으로 이동.
             }
         }
     }
 
-    private fun deleteAccount(){
+    private fun deleteAccount() {
         // 계정삭제
         UserApiClient.instance.unlink { error ->
             if (error != null) {
                 Log.e(ContentValues.TAG, "연결 끊기 실패", error)
-            }
-            else {
+            } else {
                 Log.i(ContentValues.TAG, "연결 끊기 성공. SDK에서 토큰 삭제 됨")
                 startActivity(Intent(this, MainActivity::class.java))
-                finish() //계정 삭제 성공하면 다시 앱 초기 화면으로 이동.
+                finish() // 계정 삭제 성공하면 다시 앱 초기 화면으로 이동.
             }
         }
     }
